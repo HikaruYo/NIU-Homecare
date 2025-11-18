444c00897c642cec8da3ed620bb0373a01f3bf81
HikaruYo <adithaean@gmail.com>
2025-11-18 13:39:25 +0700

    Booking FE [unfinished]

--- resources/views/components/pesan.blade.php
+++ resources/views/components/pesan.blade.php
@@ -57,6 +57,54 @@
                     </div>
                 @endforeach
             </div>
+
+            {{-- Booking --}}
+            <div class="flex mt-4 gap-4 items-center">
+                {{-- Start --}}
+                <div class="border-b-[2px] border-b-mainGray">
+                    <p>Mulai</p>
+                    <div class="flex">
+                        <div>10:30</div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <g fill="none" fill-rule="evenodd">
+                                <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                                <path fill="currentColor" d="M13.06 16.06a1.5 1.5 0 0 1-2.12 0l-5.658-5.656a1.5 1.5 0 1 1 2.122-2.121L12 12.879l4.596-4.596a1.5 1.5 0 0 1 2.122 2.12l-5.657 5.658Z" />
+                            </g>
+                        </svg>
+                    </div>
+                </div>
+
+                {{-- Arrow --}}
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
+                        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="m18 8l4 4l-4 4M2 12h20" />
+                    </svg>
+                </div>
+
+                {{-- Finished --}}
+                <div class="border-b-[2px] border-b-mainGray">
+                    <p>Selesai</p>
+                    <div class="flex">
+                        <div>11:30</div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <g fill="none" fill-rule="evenodd">
+                                <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                                <path fill="currentColor" d="M13.06 16.06a1.5 1.5 0 0 1-2.12 0l-5.658-5.656a1.5 1.5 0 1 1 2.122-2.121L12 12.879l4.596-4.596a1.5 1.5 0 0 1 2.122 2.12l-5.657 5.658Z" />
+                            </g>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Book Button --}}
+            <div class="mt-6 w-1/3">
+                <button
+                    type="submit"
+                    class="flex w-full justify-center items-center gap-3 px-5 py-2 text-lg rounded-full bg-linear-to-br from-mainColor to-thirdColor cursor-pointer hover:shadow-md transition duration-300"
+                >
+                    Pesan
+                </button>
+            </div>
         </div>
 
         {{-- Layanan --}}
